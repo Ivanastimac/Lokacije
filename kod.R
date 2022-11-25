07a0f1c9601c86b8c7c5c299cc20c9eac74626ab
istimac <istimac@istimac.lan.croz.net>
2022-11-25 12:26:08 +0100

    Popravljena csv datoteka, u kodu - povučene postaje i mjerenja tlaka zraka, učitana csv datoteka

--- kod.R
+++ kod.R
@@ -1,16 +1,29 @@
 rm(list = ls())
 
+#za scrapanje htmla
 library(rvest)
 library(stringr)
+#za funkciju xml_remove
+library(xml2)
 
-link = 'https://meteo.hr/podaci.php?section=podaci_vrijeme&param=hrvatska1_n'
-website = read_html(link)
+link <- 'https://meteo.hr/podaci.php?section=podaci_vrijeme&param=hrvatska1_n'
+website <- read_html(link)
 
-#date_time = website %>% html_nodes(".hours-browser-v2__title span") %>% html_text()
+met_zone <- website %>% html_nodes(".fd-c-table td.fd-u-text-align--left") 
 
-met_zone = website %>% html_nodes("#table-aktualni-podaci td.fd-u-text-align--left") %>% html_text() 
+#neke postaje imaju eksponent A pa to mičemo
+removed_nodes <- met_zone %>% html_nodes("sup")
+xml_remove(removed_nodes)
 
-met_zone =  str_replace_all(gsub("[\r\n]", "", met_zone), fixed(" "), "")
+met_zone <- met_zone %>% html_text() 
 
-#popravit putanju!
-file <- read.csv(file='C:/Users/istimac/Desktop/lokacije/postaje.csv', header=TRUE, encoding="UTF-8")
+met_zone <- str_replace_all(gsub("[\r\n]", "", met_zone), fixed(" "), "")
+
+pressure <- website %>% html_nodes("td:nth-child(6)") %>% html_text()
+
+#neki tlakovi imaju * pa i to mičemo
+pressure <- str_replace_all(gsub("[*]", "", pressure), fixed(" "), "")
+
+#za koristit relativnu putanju
+setwd(getwd())
+file <- read.csv(file='./postaje.csv', header=TRUE, encoding="UTF-8")
